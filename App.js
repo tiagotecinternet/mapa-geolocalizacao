eec26d4036092e59f93c6d35ce03a6095babac84
tiagotecinternet <tiago.tecinternet22@gmail.com>
2023-01-18 09:59:57 -0300

    Instalação da API da Mapas e região inicial

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const regiaoInicial = {
+    latitude: -10,
+    longitude: -55,
+    latitudeDelta: 40,
+    longitudeDelta: 40
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView style={estilos.mapa} initialRegion={regiaoInicial} />
+      </View>
+    </>
+    )
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
+  mapa: {
+    width: "100%",
+    height: "100%"
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+# Referências
+
+## 01-api-mapview
+
+**Documentação:** https://docs.expo.dev/versions/v47.0.0/sdk/map-view/
+
+### Instalação:
+
+`npx expo install react-native-maps`
