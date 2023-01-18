b7f333e97614073dfeec0c94fd25363a5cb96f4f
tiagotecinternet <tiago.tecinternet22@gmail.com>
2023-01-18 10:43:15 -0300

    Testes nas props do MapView

--- App.js
+++ App.js
@@ -3,17 +3,27 @@ import MapView from 'react-native-maps';
 
 export default function App() {
   const regiaoInicial = {
-    latitude: -10,
-    longitude: -55,
-    latitudeDelta: 40,
-    longitudeDelta: 40
+    latitude: -23.533773,
+    longitude: -46.65529,
+    // latitudeDelta: 0.0922,
+    // longitudeDelta: 0.0421
+    latitudeDelta: 10,
+    longitudeDelta: 10
   }
 
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
-        <MapView style={estilos.mapa} initialRegion={regiaoInicial} />
+        <MapView 
+          style={estilos.mapa} 
+          initialRegion={regiaoInicial} 
+          liteMode={false} // somente Android
+          mapType="standard" // satellite, hybrid, standard
+          userInterfaceStyle='dark' // somente iOS
+          maxZoomLevel={15}
+          minZoomLevel={5}
+        />
       </View>
     </>
     )
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Referências
 
+## 02-mapview-com-outras-props
+
+**Documentação com todos os detalhes da biblioteca de mapas:** https://github.com/react-native-maps/react-native-maps
+
+---
+
 ## 01-api-mapview
 
 **Documentação:** https://docs.expo.dev/versions/v47.0.0/sdk/map-view/
