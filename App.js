370513e3871e44930622beec41035cbef2f35533
tiagotecinternet <tiago.tecinternet22@gmail.com>
2023-01-19 11:41:38 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, Image, Alert } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image, Alert, Button } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from "expo-location";
 
@@ -14,10 +14,10 @@ export default function App() {
       const { status } = await Location.requestForegroundPermissionsAsync();
 
       // Verificando o status
-      if( status !== 'granted' ){
-        Alert.alert("Ops!", "Você não autorizou o uso de recursos de localização");
-        return;
-      }
+      // if( status !== 'granted' ){
+      //   Alert.alert("Ops!", "Você não autorizou o uso de recursos de localização");
+      //   return;
+      // }
 
       // Acessando os dados de geolocalização
       let localizacaoAtual = await Location.getCurrentPositionAsync({});
@@ -45,38 +45,43 @@ export default function App() {
     setLocalizacao({
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
-      latitude: event.nativeEvent.coordinate.latitude,
-      longitude: event.nativeEvent.coordinate.longitude
+      latitude: minhaLocalizacao.coords.latitude,
+      longitude: minhaLocalizacao.coords.longitude
     });
-    console.log(localizacao);
   }
 
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
-        <MapView 
-          onPress={marcarLocal}
-          style={estilos.mapa} 
-          region={localizacao ?? regiaoInicial}
-          liteMode={false} 
-          mapType="standard"
-        >
-          { localizacao &&
-            <Marker 
-              coordinate={localizacao} 
-              title="Aqui!!!"
-              onPress={ e => console.log(e.nativeEvent) }
-            />
-          }
+        <View style={estilos.viewBotao}>
+          <Button title='Onde estou?' onPress={marcarLocal} />
+        </View>
+        <View style={estilos.viewMapa}>
+          <MapView 
+            style={estilos.mapa} 
+            region={localizacao ?? regiaoInicial}
+            liteMode={false} 
+            mapType="standard"
+          >
+            { localizacao &&
+              <Marker 
+                coordinate={localizacao} 
+                title="Aqui!!!"
+                onPress={ e => console.log(e.nativeEvent) }
+              />
+            }
 
-        </MapView>
+          </MapView>
+        </View>
       </View>
     </>
     )
 }
 
 const estilos = StyleSheet.create({
+  viewMapa: { flex: 1}, 
+  viewBotao: {},
   mapa: {
     width: "100%",
     height: "100%"
